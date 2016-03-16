8a6de6329dfbe6409940f6ea1401f5fca24e55c4
codernb <cyril.halmo@students.fhnw.ch>
2016-03-16 13:57:22 +0100

    improvement

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -27,25 +27,45 @@ public class StartController : MonoBehaviour
     private MetaGesture MetaGestureScript;
     private LongPressGesture LongPressGestureScript;
     private bool Ready;
+    private bool Started;
     private int PointsCounter;
     private int PointsCollected;
+    private Vector2 ReadyPosition;
 
     void Start()
     {
         MetaGestureScript = GetComponent<MetaGesture>();
         LongPressGestureScript = GetComponent<LongPressGesture>();
         LongPressGestureScript.LongPressed += delegate {
-            Ready = true;
-            ParticleSys.SetActive(true);
-            MGameController.PlayerReady();
+            PlayerReady();
+        };
+        MetaGestureScript.TouchBegan += (sender, e) => ReadyPosition = e.Touch.Position;
+        MetaGestureScript.TouchMoved += (sender, e) => {
+            if ((e.Touch.Position - ReadyPosition).magnitude > LongPressGestureScript.DistanceLimit * 20)
+                PlayerNotReady();
         };
         MetaGestureScript.TouchEnded += delegate {
-            if (!Ready)
-                return;
-            Ready = false;
-            ParticleSys.SetActive(false);
-            MGameController.PlayerNotReady();
+            PlayerNotReady();
         };
+    }
+
+    private void PlayerReady()
+    {
+        if (Ready || Started)
+            return;
+        Ready = true;
+        ParticleSys.SetActive(true);
+        Handheld.Vibrate();
+        MGameController.PlayerReady();
+    }
+
+    private void PlayerNotReady()
+    {
+        if (!Ready)
+            return;
+        Ready = false;
+        ParticleSys.SetActive(false);
+        MGameController.PlayerNotReady();
     }
 
     public void InstantiatePoints(int number)
@@ -60,6 +80,7 @@ public class StartController : MonoBehaviour
 
     public void Go()
     {
+        Started = true;
         GetComponent<Transformer>().enabled = true;
         ParticleSys.SetActive(false);
     }
@@ -79,6 +100,7 @@ public class StartController : MonoBehaviour
             }
         } else {
             Destroy(gameObject);
+            Handheld.Vibrate();
             MGameController.PlayerDestroyed();
         }
     }
